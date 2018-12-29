3c09c0c966183a47e68a6f3ea402fb564da9097b
Drew Arrigoni <arrigoni.andrew@gmail.com>
2018-12-29 14:10:46 -0500

    Edit tags

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
   scope '/api' do
     resources :tasks, only: [:index, :create, :update, :show]
+    resources :tags, only: [:index]
   end
 end
