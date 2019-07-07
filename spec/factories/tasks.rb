4be41a45ccbaf3fadeb5b723536c0bcc37152ca9
Drew Arrigoni <arrigoni.andrew@gmail.com>
2019-07-07 15:21:15 -0400

    Gem updates; FactoryBot upgrade

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :task do
     user
-    sequence(:title) { |n| "Task Number #{n}"}
-    description ''
-    tags []
+    sequence(:title) { |n| "Task Number #{n}" }
+    description { '' }
+    tags { [] }
 
     trait :with_tags do
       tags { build_list(:tag, 3, user: user) }
